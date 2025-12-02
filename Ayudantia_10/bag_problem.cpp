28b56e60e90007d0a057b785eda1b9847fc608b1
Pascal <94018208+Paskiben@users.noreply.github.com>
2025-12-02 20:59:28 -0300

    Changed identation bag_problem

--- Ayudantia_10/bag_problem.cpp
+++ Ayudantia_10/bag_problem.cpp
@@ -1,32 +1,32 @@
 /*
-    Problema: Se tiene una maleta que soporta 100 de peso, la cual se quiere llenar con objetos que tienen un peso determinado. 
-    Se desea maximizar la cantidad de objetos en la maleta, sin exceder el peso máximo permitido.
+	Problema: Se tiene una maleta que soporta 100 de peso, la cual se quiere llenar con objetos que tienen un peso determinado. 
+	Se desea maximizar la cantidad de objetos en la maleta, sin exceder el peso máximo permitido.
 
-    La mochila se representa por la siguiente estructura:
-    
-    struct Mochila {
-        int pesoMaximo;
-        vector<int> objetos;
-    };
+	La mochila se representa por la siguiente estructura:
 
-    Donde vector<int> objetos representa los pesos de los objetos agregados a la mochila.
+	struct Mochila {
+		int pesoMaximo;
+		vector<int> objetos;
+	};
 
-    Solucion: Se debe utilizar un algoritmo para seleccionar los objetos que se pueden agregar a la mochila
-    Esto se realizará utilizando una cola de prioridad para seleccionar los objetos con menor peso primero.
-    Usted deberá completar las funciones:
-        
-    - llenarMochila(Mochila& mochila, priority_queue<int, vector<int>, greater<int>>& objetos);
-        Se debe llenar la mochila con los objetos de menor peso hasta que no se pueda agregar más sin exceder el peso máximo.
-    - crearColaDePrioridad(const vector<int>& pesos);
-        Crea una cola de prioridad a partir de un vector de pesos, ordenando los objetos de menor a mayor peso.
-    - crearObjetos(vector<int>& objetos, int n);
-        Crea un vector de objetos con pesos aleatorios entre 1 y 50, donde n es la cantidad de objetos a crear.
+	Donde vector<int> objetos representa los pesos de los objetos agregados a la mochila.
 
-    y el main para probar su funcionamiento.
-    - Se debe llamar a las funciones codificadas
-    - Se debe imprimir los objetos antes de llenar la mochila.
-    - Se debe imprimir el contenido de la mochila al final del programa.
-     
+	Solucion: Se debe utilizar un algoritmo para seleccionar los objetos que se pueden agregar a la mochila
+	Esto se realizará utilizando una cola de prioridad para seleccionar los objetos con menor peso primero.
+	Usted deberá completar las funciones:
+
+	- llenarMochila(Mochila& mochila, priority_queue<int, vector<int>, greater<int>>& objetos);
+		Se debe llenar la mochila con los objetos de menor peso hasta que no se pueda agregar más sin exceder el peso máximo.
+	- crearColaDePrioridad(const vector<int>& pesos);
+		Crea una cola de prioridad a partir de un vector de pesos, ordenando los objetos de menor a mayor peso.
+	- crearObjetos(vector<int>& objetos, int n);
+		Crea un vector de objetos con pesos aleatorios entre 1 y 50, donde n es la cantidad de objetos a crear.
+
+	y el main para probar su funcionamiento.
+	- Se debe llamar a las funciones codificadas
+	- Se debe imprimir los objetos antes de llenar la mochila.
+	- Se debe imprimir el contenido de la mochila al final del programa.
+
 */
 
 #include <iostream>
@@ -35,8 +35,8 @@
 
 struct Mochila
 {
-    int pesoMaximo;
-    std::vector<int> objetos;
+	int pesoMaximo;
+	std::vector<int> objetos;
 };
 
 void llenarMochila(Mochila& mochila, std::priority_queue<int, std::vector<int>, std::greater<int>>& objetos);
@@ -46,5 +46,5 @@ std::priority_queue<int, std::vector<int>, std::greater<int>> crearColaDePriorid
 std::vector<int> crearObjetos(std::vector<int>& objetos, int n);
 
 int main(int argc, char** argv){
-    
+
 }
